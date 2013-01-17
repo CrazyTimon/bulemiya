2eb178aaf2a9b9e2b3d2589f64d6263f5158ba37
CrazyTimon <crazytimon88@gmail.com>
2013-01-17 22:11:57 +0600

    howoldareu

--- prototype/mobilyblocks/js/init.js
+++ prototype/mobilyblocks/js/init.js
@@ -3,11 +3,10 @@ $(function(){
 	$('.socials').mobilyblocks();
 	
 	$('.nature').mobilyblocks({
-		trigger: 'hover',
 		direction: 'counter',
-		duration:500,
-		zIndex:50,
-		widthMultiplier:1.15
+		duration:1000,
+		zIndex:150,
+		widthMultiplier:1.2
 	});
 	
 });
